f7939714e89a5918dde419dafbc7d72947a4f665
erikolausson <erik@erikolausson.se>
2019-11-08 13:46:44 +0100

    Added title to gameplay window

--- src/main/java/Screen.java
+++ src/main/java/Screen.java
@@ -70,22 +70,21 @@ public class Screen {
                 System.out.println(c);
                 System.out.println(type);
 
-
-                if(c == '1'){
-                    terminal.clearScreen();
-                    return false;
-                }
-
-                else if(c == '2'){
-                    terminal.clearScreen();
-                    return true;
-                }
-
-                else if(c == 'q'){
-                    System.out.println("quit");
-                    terminal.close();
+                if(c!=null) {
+                    if (c == '1') {
+                        terminal.clearScreen();
+                        return false;
+                    } else if (c == '2') {
+                        terminal.clearScreen();
+                        return true;
+                    } else if (c == 'q') {
+                        System.out.println("quit");
+                        terminal.close();
+                        break;
+                    }
                 }
             }
+            return false;
         }
 
     public boolean botOrHumanOpponent(Terminal terminal) throws IOException, InterruptedException {
@@ -304,11 +303,11 @@ public class Screen {
 
 
 
-    public void endScreen(Terminal terminal) throws IOException, InterruptedException {
+    public void endScreen(Terminal terminal, boolean playersWin) throws IOException, InterruptedException {
 
             terminal.setForegroundColor(Main.WHITE);
 
-            if(Main.value == Main.pointsToWin){
+            if(playersWin){
                 String row1 = "You won the game!";
                 int row1Length = row1.toCharArray().length;
 
@@ -353,9 +352,10 @@ public class Screen {
             System.out.println(c);
             System.out.println(type);
 
-            if(c == 'q'){
+            if(c !=null && c == 'q'){
                 System.out.println("quit");
                 terminal.close();
+                break;
                 }
 
             }
